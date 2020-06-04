d9a9f86c1b1776bc4d0de58b7debca0939f72c14
luhao <linesahao@outlook.com>
2020-06-04 10:37:10 +0800

    CircleCI test.

--- src/main/java/com/hao/Main.java
+++ src/main/java/com/hao/Main.java
@@ -3,5 +3,6 @@ package com.hao;
 public class Main {
     public static void main(String[] args){
         System.out.println("hello, world" );
+        System.out.println("CircleCI test");
     }
 }
