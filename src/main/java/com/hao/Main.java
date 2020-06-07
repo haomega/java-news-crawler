3877412a8946ef61620ec1922d86f9c18e36cf02
luhao <linesahao@outlook.com>
2020-06-07 17:01:41 +0800

    Refactor.

--- src/main/java/com/hao/Main.java
+++ src/main/java/com/hao/Main.java
@@ -2,12 +2,10 @@ package com.hao;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -21,7 +19,7 @@ import java.util.List;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         List<String> linkPool = new ArrayList<>();
         linkPool.add("https://news.sina.cn/");
         Set<String> processedLinks = new HashSet<>();
@@ -40,33 +38,13 @@ public class Main {
             //是否是我们感兴趣的link
             if (isInserestedLink(link)) {
                 System.out.println("link: " + link);
-                CloseableHttpClient httpclient = HttpClients.createDefault();
-                HttpGet httpGet = new HttpGet(link);
-                try(CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-                    System.out.println(response1.getStatusLine());
-                    HttpEntity entity = response1.getEntity();
+                Document doc = httpGetAndParseToDoc(link);
+                // 将页面上所有的<a>连接放入pool中
+                List<Element> aTags = doc.select("a");
+                addLinkToPool(linkPool, aTags);
 
-                    Document doc = Jsoup.parse(IOUtils.toString(entity.getContent(), StandardCharsets.UTF_8));
-                    // 将页面上所有的<a>连接放入pool中
-                    List<Element> aTags = doc.select("a");
-                    for (Element e : aTags) {
-                        String href = e.attr("href");
-                        if (href.startsWith("/") || href.startsWith("#") || href.isEmpty() || href.contains("javascript")) {
-                            continue;
-                        }
-                        if (href.contains("news.sina")) {
-                            linkPool.add(href);
-                        }
-                    }
-
-                    if (isNewsPage(doc)) {
-                        System.out.println(doc.selectFirst("h1").text());
-                    }
-                    // do something useful with the response body
-                    // and ensure it is fully consumed
-                    EntityUtils.consume(entity);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if (isNewsPage(doc)) {
+                    System.out.println(doc.selectFirst("h1").text());
                 }
                 processedLinks.add(link);
             }
@@ -74,15 +52,50 @@ public class Main {
         }
     }
 
+    private static void addLinkToPool(List<String> linkPool, List<Element> aTags) {
+        for (Element e : aTags) {
+            String href = e.attr("href");
+            if (href.startsWith("/") || href.startsWith("#") || href.isEmpty() || href.contains("javascript")) {
+                continue;
+            }
+            if (href.contains("news.sina")) {
+                linkPool.add(href);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseToDoc(String link) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(link);
+        try(CloseableHttpResponse response = httpclient.execute(httpGet)) {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            return Jsoup.parse(IOUtils.toString(entity.getContent(), StandardCharsets.UTF_8));
+        }
+    }
+
+
     private static boolean isNewsPage(Document document) {
         Elements articleBody = document.select("article");
         return !articleBody.isEmpty();
     }
 
     private static boolean isInserestedLink(String link) {
+        return isSinaNewsPage(link) && isNotPassportLink(link) && isNotShtml(link);
+
+
+    }
+
+    private static boolean isNotShtml(String link) {
+        return !link.endsWith("shtml");
+    }
+
+    private static boolean isNotPassportLink(String link) {
+        return !link.contains("passport");
+    }
+
+    private static boolean isSinaNewsPage(String link) {
         return link.startsWith("https://news.sina")
-                || link.startsWith("https://news.sina")
-                || !link.endsWith("shtml")
-                && !link.contains("passport");
+                || link.startsWith("https://news.sina");
     }
 }
