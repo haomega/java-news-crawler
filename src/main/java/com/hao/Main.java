b84fe0e9b724d15449bb529c0f6f9bc6f782a3f0
luhao <linesahao@outlook.com>
2020-06-09 19:37:50 +0800

    使用H2数据库。

--- src/main/java/com/hao/Main.java
+++ src/main/java/com/hao/Main.java
@@ -6,6 +6,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.h2.jdbc.JdbcBlob;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,25 +14,24 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import java.sql.*;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        List<String> linkPool = new ArrayList<>();
-        linkPool.add("https://news.sina.cn/");
-        Set<String> processedLinks = new HashSet<>();
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+        Class.forName("org.h2.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:h2:file:E:\\hcsp-java\\java-news-crawler\\src\\main\\resources\\db\\h2\\news",
+                "root",
+                "root");
 
-        while (true) {
-            if (linkPool.size() == 0) {
-                break;
-            }
-            String link = linkPool.remove(linkPool.size() - 1);
+        Set<String> processedLinks = new HashSet<>();
+        String link;
+        while ((link = getToBeProcessedLinkAndRemove(connection)) != null) {
 
             //判断link是否被处理过
-            if (processedLinks.contains(link)) {
+            if (isLinkProcessed(connection, link)) {
                 continue;
             }
 
@@ -41,25 +41,67 @@ public class Main {
                 Document doc = httpGetAndParseToDoc(link);
                 // 将页面上所有的<a>连接放入pool中
                 List<Element> aTags = doc.select("a");
-                addLinkToPool(linkPool, aTags);
+                storeLinkToBeProcess(connection, aTags);
 
                 if (isNewsPage(doc)) {
-                    System.out.println(doc.selectFirst("h1").text());
+                    storeNewToDatabase(connection, doc);
                 }
-                processedLinks.add(link);
+                addLintToProcessed(connection, link);
             }
 
         }
     }
 
-    private static void addLinkToPool(List<String> linkPool, List<Element> aTags) {
+    private static void addLintToProcessed(Connection connection, String link) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("delete from LINK_ALREADY_PROCESSED where link = ?");
+        preparedStatement.setString(1, link);
+        preparedStatement.executeUpdate();
+    }
+
+    private static void storeNewToDatabase(Connection connection, Document doc) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into NEWS (title, content, created_at, updated_at) values (?, ?, now(), now())");
+        String title = doc.select("h1").text();
+        String content = doc.select(".art_content").text();
+        preparedStatement.setString(1, title);
+        preparedStatement.setString(2, content);
+        preparedStatement.executeUpdate();
+    }
+
+    private static boolean isLinkProcessed(Connection connection, String link) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("select count(1) from LINK_ALREADY_PROCESSED where link = ?");
+        preparedStatement.setString(1, link);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        int count = resultSet.getInt(1);
+        return  count != 0;
+    }
+
+    private static String getToBeProcessedLinkAndRemove(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select link from LINK_TOBE_PROCESSED limit 1");
+        if (resultSet.next()) {
+            String link =  resultSet.getString(1);
+            if (link != null) {
+                PreparedStatement preparedStatement = connection.prepareStatement("delete from LINK_TOBE_PROCESSED where link = ?");
+                preparedStatement.setString(1, link);
+                preparedStatement.executeUpdate();
+            }
+            return link;
+        }
+        return null;
+    }
+
+    private static void storeLinkToBeProcess(Connection connection, List<Element> aTags) throws SQLException {
+        String insertSql = "insert into LINK_TOBE_PROCESSED (link) values (?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(insertSql);
         for (Element e : aTags) {
             String href = e.attr("href");
             if (href.startsWith("/") || href.startsWith("#") || href.isEmpty() || href.contains("javascript")) {
                 continue;
             }
             if (href.contains("news.sina")) {
-                linkPool.add(href);
+                preparedStatement.setString(1, href);
+                preparedStatement.executeUpdate();
             }
         }
     }
